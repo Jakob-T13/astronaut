d8bc9e5ac16bf314e1b9cf58af90eb03adceb2fd
Jakob Thunen <jthunen@dev-10.com>
2022-04-12 11:31:00 -0500

    initial commit of astronaut class code

---
+++ astronaut-code.py
@@ -0,0 +1,9 @@
+import csv
+
+class Astronaut:
+    def __init__(self,name,flighthrs,status):
+        self.__name = name
+        self.__flighthrs = flighthrs
+        self.__status = status
+    
+    
