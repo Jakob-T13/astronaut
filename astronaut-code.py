82a0185d1df39a08001115d0267f19957e2dd28a
Jakob Thunen <jthunen@dev-10.com>
2022-04-12 11:50:19 -0500

    read csv and put relevant fields into a list of Astronaut objects

--- astronaut-code.py
+++ astronaut-code.py
@@ -23,3 +23,13 @@ class Astronaut:
         
     def __str__(self):
         return f"{self.name}, {self.status}"
+        
+f = open("astronauts.csv")
+csvreader = csv.reader(f)
+next(csvreader)     #skip the header row of the csv file
+astronaut_lst = []
+for i in csvreader:
+    astro = Astronaut(i[0],int(i[13]),i[3])
+    astronaut_lst.append(astro)
+    
+print(str(astronaut_lst[0]))
